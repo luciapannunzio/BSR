7aaaba3e60c17665b35171e3b289463cc29b8339
lucia pannunzio <luciapannunzio@gmail.com>
2025-02-24 00:32:03 +0100

    Add API folder and restore db.json

--- api/db.js
+++ api/db.js
@@ -1,9 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'public', 'db.json');
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  const data = JSON.parse(fileContent);
-  res.status(200).json(data);
+export default async function handler(req, res) {
+  const filePath = path.join(process.cwd(), 'public/db.json');
+  if (fs.existsSync(filePath)) {
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const data = JSON.parse(fileContent);
+    res.status(200).json(data);
+  } else {
+    res.status(404).json({ message: 'db.json not found' });
+  }
 }
